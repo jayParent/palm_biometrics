392be894ef5a3952eac3fec84e12df958c55aa2f
Jerome <jeromeparent9@gmail.com>
2020-09-22 21:50:59 -0400

    import all data and get lbp

--- functions.py
+++ functions.py
@@ -7,18 +7,16 @@ import math
 import skimage.data
 import skimage.segmentation as seg
 from skimage import filters, transform
-from skimage import draw
-from skimage import color
 from skimage import exposure, io, feature, measure
-from skimage.color import rgb2gray, label2rgb
-from skimage.feature import hog, canny, local_binary_pattern, ORB, plot_matches, match_descriptors
-from skimage.filters import sobel, prewitt, roberts
+from skimage.feature import hog, canny, local_binary_pattern
 from skimage.transform import rescale, resize
 from skimage.util import img_as_ubyte
-from skimage.measure import find_contours, approximate_polygon, subdivide_polygon, label, regionprops
-from skimage.morphology import closing, square
-from skimage.draw import rectangle, rectangle_perimeter
+from skimage.measure import find_contours, label, regionprops
 from scipy import ndimage as ndi
+
+def importAllCollection():
+    img_list = io.ImageCollection('./palms_data')
+    return img_list
 
 def importCollection(collection):
     img_list = io.ImageCollection('./palms_data/' + collection + '/*.jpg')
@@ -69,6 +67,101 @@ def saveImage(image, index, collection):
     plt.imshow(image, cmap='gray')
     plt.savefig('palms_cropped/' + collection + '/flood_fill' + str(index) + '.jpg')
 
+def getRoiAndLbp(image):
+    mask = image > 50
+    edges = feature.canny(mask)
+
+    contours = measure.find_contours(edges, 0.8, fully_connected='high')
+
+    # Find longest contour
+    max_length = 0
+    for contour in contours:
+        if len(contour) > max_length:
+            max_length = len(contour)
+            longest_contour = contour
+
+    longest_contour_min_y = image.shape[0]
+    longest_contour_max_y = 0
+
+    # Rotate image if its horizontal
+    for point in longest_contour:
+        if point[0] < longest_contour_min_y:
+            longest_contour_min_y = point[0]
+        if point[0] > longest_contour_max_y:
+            longest_contour_max_y = point[0]
+
+    if longest_contour_max_y - longest_contour_min_y < image.shape[0]:
+        image = img_as_ubyte(transform.rotate(image, -80))
+
+    mask = image > 50
+    edges = feature.canny(mask)
+
+    contours = measure.find_contours(edges, 0.8, fully_connected='high')
+
+    max_point = np.array([])
+    max_points = []
+    for i, contour in enumerate(contours):
+        if len(contour) < 700:
+            max_mean = 0
+            for p in contour:
+                if p.mean() > max_mean:
+                    max_point = p
+            max_points.append(max_point)
+
+    # Landmarks
+    max_points = [max_points[0], max_points[-1]]
+    landmark_1 = np.array(max_points[0])
+    landmark_2 = np.array(max_points[1])
+
+    x1 = landmark_1[1]
+    y1 = landmark_1[0]
+    x2 = landmark_2[1]
+    y2 = landmark_2[0]
+
+    landmark_distance = math.hypot(x2 - x1, y2 - y1)
+    distance_from_fingers = int(image.shape[0] / 8)
+
+    # Points slightly under landmark line, start of ROI
+    top_left_point = np.array([y1 - distance_from_fingers, x1])
+    top_right_point = np.array([y2 - distance_from_fingers, x2])
+    
+    if top_left_point[0] < landmark_1[0]:
+        top_left_point = np.array([y1 + distance_from_fingers, x1])
+        top_right_point = np.array([y2 + distance_from_fingers, x2])
+
+    # Bottom points, end of ROI 
+    dx = top_left_point[1] - top_right_point[1]
+    dy = top_left_point[0] - top_right_point[0]
+    dx /= landmark_distance
+    dy /= landmark_distance
+    x3 = top_left_point[1] + (landmark_distance) * dy
+    y3 = top_left_point[0] - (landmark_distance) * dx
+    x4 = top_right_point[1] + (landmark_distance) * dy
+    y4 = top_right_point[0] - (landmark_distance) * dx
+    
+    bottom_left_point = np.array([y3, x3])
+    bottom_right_point = np.array([y4, x4])
+
+    try:
+        # Crop image to 128, 128. Region of interest
+        miny = int(top_left_point[0])
+        maxy = int(bottom_right_point[0])
+        minx = int(bottom_left_point[1])
+        maxx = int(top_right_point[1])
+
+        roi = transform.resize(image[miny:maxy, minx:maxx], (128, 128))
+        roi = img_as_ubyte(roi)
+        
+        # Local Binary Pattern
+        radius = 1
+        n_points = 8 * radius
+
+        lbp = feature.local_binary_pattern(roi, n_points, radius, method='default')
+        
+        return lbp
+    except:
+        return 0
+
 def findLandmarks(image, index, collection):
     fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(10, 15))
 
@@ -105,7 +198,6 @@ def findLandmarks(image, index, collection):
     max_point = np.array([])
     max_points = []
     for i, contour in enumerate(contours):
-        # print(f'contour: {i}', len(contour))
         if len(contour) < 700:
             max_mean = 0
             for p in contour:
@@ -171,7 +263,6 @@ def findLandmarks(image, index, collection):
     maxy = int(bottom_right_point[0])
     minx = int(bottom_left_point[1])
     maxx = int(top_right_point[1])
-    print(top_left_point)
 
     roi = transform.resize(image[miny:maxy, minx:maxx], (128, 128))
     roi = img_as_ubyte(roi)
@@ -197,8 +288,8 @@ def findLandmarks(image, index, collection):
     ax3.imshow(roi, cmap='gray')
     ax4.imshow(lbp, cmap='gray')
     # ax4.hist(roi.ravel(), bins=256, histtype='step', color='black')
-    ax5.hist(lbp.ravel(), bins='auto', histtype='step', color='black')
+    ax5.hist(image.ravel(), bins='auto', histtype='step', color='black')
     # ax5.imshow(roi_rescaled, cmap='gray')
     # ax6.imshow(lbp, cmap='gray')
-    ax6.hist(lbp.ravel(), bins=256, histtype='step', color='black')
+    ax6.hist(image.ravel(), bins=256, histtype='step', color='black')
     plt.savefig('landmarks/' + collection + '/' + str(index) + '.jpg')
--- ver1.py
+++ ver1.py
@@ -8,19 +8,34 @@ from scipy import ndimage as ndi
 from skimage import feature, transform
 from skimage import util, io, exposure, img_as_float, filters, segmentation, morphology, color, measure, img_as_ubyte
 from skimage.measure import label
-from functions import importCollection, findLandmarks, saveImage
+from functions import importCollection, findLandmarks, saveImage, getRoiAndLbp, importAllCollection
 
-# collection = '0005'
+# collection = '0189'
 # img_collection = importCollection(collection)
 # for i, img in enumerate(img_collection):
 #     findLandmarks(img, i, collection)
-    
 
 
-palm_filename = os.path.join(skimage.data_dir, '0001_m_l_02.jpg')
-palm = img_as_ubyte(io.imread(palm_filename, as_gray=True))
+files = os.listdir('./palms_data')
+img_collections = []
 
-findLandmarks(palm, '3', 'test')
+for fi in files:
+    img_collection = io.ImageCollection(f'./palms_data/{fi}/*.jpg')
+    img_collections.append(img_collection)
+
+for i, collection in enumerate(img_collections):
+    for j, img in enumerate(collection):
+        try:
+            lbp = getRoiAndLbp(img)
+        except:
+            pass
+        
+
+
+# palm_filename = os.path.join(skimage.data_dir, '0001_m_l_02.jpg')
+# palm = img_as_ubyte(io.imread(palm_filename, as_gray=True))
+
+# roi, lbp = getLbp(palm)
 
 
 
