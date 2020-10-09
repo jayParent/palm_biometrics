ef85ad7a2cfd93f8281f9c769a754d318540ce92
Jerome <jeromeparent9@gmail.com>
2020-10-08 20:42:48 -0400

    working?

--- example.py
+++ example.py
@@ -4,24 +4,27 @@ from PROIE import *
 from functions import *
 from skimage import io
 from skimage.feature import hog
+from skimage.transform import resize
+from sklearn.decomposition import PCA
 import os
+import pickle
 
 if __name__ == '__main__':
 
-    image = io.imread('./resources/palmprint_roi.jpg')
-    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(8, 8),
-                    cells_per_block=(1, 1), visualize=True, multichannel=False)
-    plt.imshow(hog_image, cmap='gray')
-    plt.savefig('./resources/hog1.jpg')
+    # image = io.imread('./resources/palmprint_roi.jpg')
+    # fd, hog_image = hog(image, orientations=8, pixels_per_cell=(8, 8),
+    #                 cells_per_block=(1, 1), visualize=True, multichannel=False)
+    # plt.imshow(hog_image, cmap='gray')
+    # plt.savefig('./resources/hog1.jpg')
 
-    def get_and_save_roi(folder_path):
-        files = os.listdir(folder_path)
+    def get_and_save_roi(folder):
+        files = os.listdir(folder)
 
         for i, f in enumerate(files):
-            for img in os.listdir(f'./{folder_path}/{f}'):
+            for img in os.listdir(f'./{folder}/{f}'):
                 try:
                     proie = PROIE()
-                    proie.extract_roi(f'{folder_path}/{f}/{img}', rotate=True)
+                    proie.extract_roi(f'{folder}/{f}/{img}', rotate=True)
                     proie.save(f'./dataset/{i+1}/{img}.jpg')
                 except:
                     continue
@@ -38,19 +41,71 @@ if __name__ == '__main__':
                 lbps.append(lbp)
         
         return lbps
+    
+    def save_add_labels_and_side(folder):
+        files = os.listdir(folder)
+
+        labels = []
+        hand_sides = []
+        data = []
+
+        for i, f in enumerate(files):
+            ic = io.ImageCollection(f'./{folder}/{f}/*.jpg')
+
+            for img_name in os.listdir(f'./{folder}/{f}'):
+                if 'r' in img_name:
+                    hand_side = 'r'
+                else:
+                    hand_side = 'l'
+
+            for img in ic:
+                if img.shape[0] >= 128 and img.shape[1] >= 128:
+                    img = transform.resize(img, (128, 128))
+                    fd = hog(img, orientations=8, pixels_per_cell=(8,8), cells_per_block=(1,1))
+
+                    data.append(fd)
+                    labels.append(i+1)
+                    hand_sides.append(hand_side)
+
+
+        with open('labels.txt', 'wb') as fp:
+            pickle.dump(labels, fp)
+        with open('hand_sides.txt', 'wb') as fp:
+            pickle.dump(hand_sides, fp)
+        with open('data.txt', 'wb') as fp:
+            pickle.dump(data, fp)
 
     def filter_bad_roi_get_hog(folder):
         img_collection  = io.ImageCollection(f'./{folder}/*/*.jpg')
         hogs = []
         
         for img in img_collection:
-            if img.shape[0] < 100 or img.shape[1] < 100:
+            if img.shape[0] < 128 or img.shape[1] < 128:
                 continue
             else:
+                img = transform.resize(img, (128, 128))
                 fd = hog(img, orientations=8, pixels_per_cell=(8,8), cells_per_block=(1,1))
                 hogs.append(fd)
         
         return hogs
 
-    # lbps = filter_bad_roi_get_lbp('dataset')
-    # get_and_save_roi('./palms_data')
+    def get_feature_descriptors(palms):
+        fds = []
+
+        for palm in palms:
+            fd = hog(palm, orientations=8, pixels_per_cell=(8,8), cells_per_block=(1,1))
+            fds.append(fd)
+
+        with open('hog_fds.txt', 'wb') as fp:
+            pickle.dump(fds, fp)
+
+    # with open('palms.txt', 'rb') as fp:
+    #     palms = pickle.load(fp)
+    # with open('labels.txt', 'rb') as fp:
+    #     labels = pickle.load(fp)
+    # with open('hand_sides.txt', 'rb') as fp:
+    #     hand_sides = pickle.load(fp)
+    # with open('hog_fds.txt', 'rb') as fp:
+    #     hog_fds = pickle.load(fp)
+    # with open('data.txt', 'rb') as fp:
+    #     data = pickle.load(fp)
--- ml.py
+++ ml.py
@@ -6,33 +6,25 @@ import pickle
 from numpy import load
 from skimage import io
 from sklearn.model_selection import train_test_split
-
-collection = os.listdir('./dataset')
-
-data = []
-labels = []
-
-# for folder in collection:
-#     filenames = os.listdir(f'./dataset/{folder}')
-#     for fname in filenames:
-#         try:
-#             lbp = np.load(f'./dataset/{folder}/{fname}', allow_pickle=True)
-#             if lbp.shape == (128, 128):
-#                 data.append(lbp)
-#                 labels.append(folder)
-#             else:
-#                 continue
-#         except:
-#             continue
-
-# with open('data.txt', 'wb') as fp:
-#     pickle.dump(data, fp)
-# with open('labels.txt', 'wb') as fp:
-#     pickle.dump(labels, fp)
+from sklearn.svm import SVC
+from sklearn import datasets
+from sklearn.decomposition import PCA
 
 with open('data.txt', 'rb') as fp:
     data = pickle.load(fp)
 with open('labels.txt', 'rb') as fp:
     labels = pickle.load(fp)
 
-lbps_train, lbps_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.1, random_state=42)
+data = np.array(data)
+y = np.array(labels)
+
+pca = PCA(n_components=150)
+pca.fit(data)
+
+X = pca.fit_transform(data)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+clf = SVC(kernel='poly', degree=4, coef0=1, C=5)
+clf.fit(X_train, y_train)
+print(clf.score(X_train, y_train))
