e9eb1c72f9999aa6c1c9674b014cc24dcd4f40b7
Jerome <jeromeparent9@gmail.com>
2020-12-07 14:43:41 -0500

    added requirements.txt

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     group = parser.add_argument_group('Test')
     group.add_argument(
-        '-t', '--test', help='Run project example using provided images.')
+        '-t', '--test', help='Run project example using provided images.', action='store_true')
 
     group = parser.add_argument_group('Real')
     parser.add_argument(
@@ -19,11 +19,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if (args.test):
-        subjects = import_data(data_filename)
-        good_subjects = filter_and_pca_subjects(subjects, 8)
-        classifiers = create_classifiers(good_subjects)
+        if(args.build):
+            create_dataset_folders(
+            args.build, 'test_dataset', 'test_oneHand_data')
+            get_and_save_roi(args.build, 'test_dataset')
+            save_data_one_class_one_hand('test_dataset', 'test_oneHand_data')
+        else:
+            subjects = import_data(data_filename)
+            good_subjects = filter_and_pca_subjects(subjects, 8)
+            classifiers = create_classifiers(good_subjects)
 
-        test_dataset(good_subjects, classifiers)
+            test_dataset(good_subjects, classifiers)
 
     elif (args.build):
         create_dataset_folders(
